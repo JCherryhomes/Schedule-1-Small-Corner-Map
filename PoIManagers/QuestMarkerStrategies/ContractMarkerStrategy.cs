4864a02d8c02cd08f06719916f39c8a72dccd689
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2025-11-22 00:32:34 -0700

    Updated strategies with the interace and base class changes

---
+++ PoIManagers/QuestMarkerStrategies/ContractMarkerStrategy.cs
@@ -0,0 +1,86 @@
+﻿using MelonLoader;
+using Small_Corner_Map.Helpers;
+using Small_Corner_Map.Main;
+
+#if IL2CPP
+using Il2CppScheduleOne.Quests;
+#else
+using ScheduleOne.Quests;
+#endif
+
+namespace Small_Corner_Map.PoIManagers.QuestMarkerStrategies
+{
+    internal class ContractMarkerStrategy : PoIManagerBase<Quest>, IPoIManager<Quest>
+    {
+        public ContractMarkerStrategy(MinimapContent mapContent, MapPreferences preferences, MarkerRegistry registry) : base(mapContent, preferences, registry)
+        {
+            MarkerKeyPrefix = "Contract_Marker";
+        }
+
+        public void AddMarker(Quest quest)
+        {
+            var contract = quest as Contract;
+            if (contract is null)
+            {
+                MelonLogger.Msg("Could not cast quest object to Contract");
+                return;
+            }
+            
+            if (contract == null || !contract.name.StartsWith(Constants.ContractQuestName)) return;
+            if (!contract.IsTracked || contract.State != EQuestState.Active) return;
+            if (contract.DeliveryLocation?.CustomerStandPoint == null) return;
+            
+            var markerData = GetMarkerData(contract);
+            Registry.AddOrUpdateMarker(markerData);
+        }
+
+        public void AddAllMarkers()
+        {
+            var contractContainer = QuestManager.Instance.ContractContainer;
+            var contracts = contractContainer.GetComponentsInChildren<Contract>();
+            foreach (var contract in contracts)
+            {
+                AddMarker(contract);
+            }
+        }
+
+        public void RemoveAllMarkers()
+        {
+            var contractContainer = QuestManager.Instance.ContractContainer;
+            var contracts = contractContainer.GetComponentsInChildren<Contract>();
+            foreach (var contract in contracts)
+            {
+                RemoveMarker(contract);
+            }
+        }
+
+        internal override MarkerRegistry.MarkerData GetMarkerData(Quest contract)
+        {
+            var worldPos = ((Contract)contract).DeliveryLocation.CustomerStandPoint.position;
+            return new MarkerRegistry.MarkerData
+            {
+                Id = GetMarkerName(contract),
+                WorldPos = worldPos,
+                IconPrefab = contract.IconPrefab?.gameObject,
+                Type = MarkerType.Contract,
+                DisplayName = contract.name,
+                XOffset = -Constants.MarkerXOffset,
+                ZOffset = -Constants.MarkerZOffset,
+                IsTracked = contract.IsTracked,
+                IsVisibleOnMinimap = true,
+                IsVisibleOnCompass = true
+            };
+        }
+
+        internal override void CachePoIIcon(Quest contract)
+        {
+            if (contract == null || contract.IconPrefab == null) return;
+            IconPrefab = contract.IconPrefab.gameObject;
+        }
+
+        public void RemoveMarker(Quest quest)
+        {
+            Registry.RemoveMarker(GetMarkerName(quest));
+        }
+    }
+}
--- PoIManagers/QuestMarkerStrategies/QuestMarkerStrategyResolver.cs
+++ PoIManagers/QuestMarkerStrategies/QuestMarkerStrategyResolver.cs
@@ -1,21 +1,21 @@
-﻿#if IL2CPP
+﻿using Small_Corner_Map.Helpers;
+using Small_Corner_Map.Main;
+
+#if IL2CPP
 using Il2CppScheduleOne.Quests;
 #else
 using ScheduleOne.Quests;
 #endif
 
-using Small_Corner_Map.Helpers;
-using Small_Corner_Map.Main.QuestMarkerStrategies;
-
-namespace Small_Corner_Map.Main.QuestMarkerStrategies
+namespace Small_Corner_Map.PoIManagers.QuestMarkerStrategies
 {
     internal static class QuestMarkerStrategyResolver
     {
-        public static IQuestMarkerStrategy GetStrategy(MinimapContent minimapContent, MapPreferences preferences, string key, MarkerRegistry registry)
+        public static IPoIManager<Quest> GetStrategy(MinimapContent minimapContent, MapPreferences preferences, string key, MarkerRegistry registry)
         {
             if (key == "Contract")
                 return new ContractMarkerStrategy(minimapContent, preferences, registry);
-            if (key == QuestMarkerStrategyBase.DeadDropQuestName)
+            if (key == Constants.DeadDropQuestName)
                 return new DeadDropQuestMarkerStrategy(minimapContent, preferences, registry);
             return new RegularQuestMarkerStrategy(minimapContent, preferences, registry);
         }
