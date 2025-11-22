15a3cc9a4cb17f8907091947be099401ef788069
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2025-11-22 00:31:59 -0700

    Refactored the quest manager interface and base class to make them generic

---
+++ PoIManagers/PoIManagerBase.cs
@@ -0,0 +1,40 @@
+﻿using MelonLoader;
+using Small_Corner_Map.Helpers;
+using Small_Corner_Map.Main;
+using UnityEngine;
+
+namespace Small_Corner_Map.PoIManagers;
+
+public abstract class PoIManagerBase<T>
+{
+    public static GameObject IconPrefab { get; set; }
+
+    internal string MarkerKeyPrefix { get; set; }
+    internal MinimapContent MapContent { get; }
+    internal MapPreferences Preferences { get; }
+    internal MarkerRegistry Registry { get; }
+    internal GameObject MapContentObject => MapContent.MapContentObject;
+
+    public PoIManagerBase(MinimapContent mapContent, MapPreferences preferences, MarkerRegistry registry)
+    {
+        MapContent = mapContent;
+        Preferences = preferences;
+        Registry = registry;
+    }
+    
+    protected virtual string GetMarkerName(T marker)
+    {
+        var guidProperty = marker.GetType().GetProperty("GUID");
+        if (guidProperty == null)
+        {
+            MelonLogger.Msg("GetMarkerName: The type " + marker.GetType().Name + " does not have a GUID property.");
+            throw new MissingMemberException("You may need to override GetMarkerName in the derived class.");
+        }
+
+        return MarkerKeyPrefix + "_" + guidProperty.GetValue(marker);
+    }
+
+    internal abstract MarkerRegistry.MarkerData GetMarkerData(T marker);
+
+    internal abstract void CachePoIIcon(T marker);
+}
