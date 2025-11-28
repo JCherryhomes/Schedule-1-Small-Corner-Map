80b79d5506534b28814cfa4d478c0aa354b3f941
Jonathan Cherryhomes <cherryhomesj@gmail.com>
2025-11-28 11:34:39 -0700

    Reapply "Reapply "Reapply "feat(minimap): Scale down map image and adjust coordinate system"""
    This reverts commit 5dd9d4893823c2e6e83c77ea4ba35bb1a03376cd.

--- PoIManagers/MinimapContentManager.cs
+++ PoIManagers/MinimapContentManager.cs
@@ -1,19 +1,32 @@
-using Small_Corner_Map.Helpers;
+using MelonLoader;
+using System.Collections;
 using UnityEngine;
+
+using Small_Corner_Map.Helpers;
 
 namespace Small_Corner_Map.PoIManagers
 {
+    [RegisterTypeInIl2Cpp]
     public class MinimapContentManager : MonoBehaviour
     {
         private RectTransform _mapContent;
         private Transform _playerTransform;
         private MinimapCoordinateSystem _coordinateSystem;
 
-        public void Initialize(RectTransform mapContent, Transform playerTransform)
+        public IEnumerator Initialize(RectTransform mapContent, Transform playerTransform)
         {
+            if (mapContent == null || playerTransform == null)
+            {
+                yield return new WaitForSeconds(1.0f);
+            }
+            
             _mapContent = mapContent;
             _playerTransform = playerTransform;
             _coordinateSystem = new MinimapCoordinateSystem();
+        }
+        
+        void Start()
+        {
         }
 
         void Update()
